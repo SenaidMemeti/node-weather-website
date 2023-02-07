7a01a14dc66a10d1b70b39f5939d0a230d443c5b
SenaidMemeti <senaiidd@hotmail.com>
2023-02-07 15:35:33 +0100

    r

--- src/app.js
+++ src/app.js
@@ -49,7 +49,7 @@ app.get('/about', (req, res) =>{
 })
 
 app.get('/help', (req, res) =>{
-    res.render('/help',{
+    res.render('help',{
         title: 'Help',
         helpText:'Senaid Memeti aide est là',
         name: 'Senaid Memeti'
